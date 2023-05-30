90c56c2f5d954e0aeae51060227122f11c27b843
jvin08 <jvin08@gmail.com>
2023-05-29 19:29:20 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# shopping_cart
-create and update shopping list
+### - viewport 
+
+# - https://www.youtube.com/watch?v=SElOdK2qTpI, 
+# - https://courses.kevinpowell.co/conquering-responsive-layouts
+
+# - mobile friendly
+
+### - non selecting
+
+# - user-select: none;
+
+### - background edit
+# - https://www.fotor.com/features/background-remover/upload
+
+# - import functions from firebase
+# - firebase initializeApp function 
+
+# - firebase getDatabase(app) 
+creating database object and fed to it app object
+# - firebase ref(database, "cart");
+we make "cart" reference a place to hold items
+# - push(shoppingListInDB, element)
+push function to add elements to firebase
+# - onValue(shoppingListInDB, callback function(snapshot))
+this function listen on changes in database, when it gets snapshot it runs code again
+# - IDs 
+# - remove(ID)
+# - Object --> to Array
+# - createElement 
+to create element instead of using innerHTML
+# - flexbox: flex-wrap
+lets us wrap elements inside parent el
+# - flexbox: gap
+easily fill gap between items
+# - user-select
+avoid selecting by user elements
+# - setting viewport
+width of the app would be correct on the mobile devices
+# - favicon.io
+adding icons images
+# - web application manifest
+let us transform our app to our phones like a native app
+
---
+++ index.js
@@ -0,0 +1,101 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://shoppingcart-database-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+
+const shoppingListInDB = ref(database, "cart");
+const cartSample = [
+    "ягоди",
+    "яблука",
+    "огірки",
+    "банани",
+    "апельсини",
+    "зелень",
+    "морква",
+    "картопля",
+    "консерви",
+    "хліб",
+    "масло",
+    "сметана",
+    "вода",
+    "печиво",
+    "йогурт",
+    "pepsi",
+    "чай",
+    "тарілки",
+    "вологі серветки",
+    "сухі серветки",
+];
+
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-to-cart");
+const shoppingCartEl = document.getElementById("shopping-cart");
+const cartSampleEl = document.getElementById("cart-sample");
+
+const clearInputFieldEl = () => inputFieldEl.value = "";
+
+const addItemToShoppingCartEl = (item) => {
+        // shoppingCartEl.innerHTML += `<li>${itemValue}</li>`;
+        let itemID = item[0]
+        let itemValue = item[1]
+        let newElement = document.createElement("li");
+        newElement.textContent = itemValue;
+
+        shoppingCartEl.append(newElement);
+        newElement.addEventListener("click", function(){
+
+            let exactLocationOfItemInDB = ref(database, `cart/${itemID}`)
+
+            remove(exactLocationOfItemInDB);
+
+        })
+}
+
+const clearShoppingCartEl = () => {
+    shoppingCartEl.innerHTML = "";
+}
+
+onValue(shoppingListInDB, function(snapshot){
+    if(snapshot.exists()){
+        let cartItems = Object.entries(snapshot.val())
+
+        clearShoppingCartEl();
+    
+        for (let i = 0; i < cartItems.length; i++) {
+            let item = cartItems[i];
+            addItemToShoppingCartEl(item)
+        }
+    } else {
+        shoppingCartEl.innerHTML = "Кошик пустий ще . . ."; 
+        let newElement = document.createElement("span");
+        newElement.setAttribute("id", "cart-sample")
+        newElement.textContent = "Простий кошик";
+        shoppingCartEl.append(newElement);
+        // generating cart sample
+        newElement.addEventListener("click", function(){
+            for (let id = 0; id < cartSample.length; id++) {
+                const element = cartSample[id];
+                push(shoppingListInDB, element);
+            }
+            clearInputFieldEl();
+        })
+         
+    }
+    
+})
+
+
+addButtonEl.addEventListener("click", function(){
+
+    let inputValue = inputFieldEl.value;
+    
+    inputValue && push(shoppingListInDB, inputValue);
+
+    clearInputFieldEl();
+})
+
